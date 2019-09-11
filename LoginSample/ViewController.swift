e8fadaff43b19b5349280a4c9740fa6dc6423652
Roberto Machorro <RobertoMachorro@users.noreply.github.com>
2019-09-11 15:34:10 -0400

    Initial Commit

---
+++ LoginSample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LoginSample
+//
+//  Created by Roberto Machorro on 9/11/19.
+//  Copyright © 2019 Roberto Machorro. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
