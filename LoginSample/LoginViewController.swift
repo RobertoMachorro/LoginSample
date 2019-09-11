711441dd468260f1a1246929db1ab6cf1cba40eb
Roberto Machorro <RobertoMachorro@users.noreply.github.com>
2019-09-11 15:48:17 -0400

    Setup view controllers classes and in Storyboard. The std ViewController for the logged in window and LoginViewController for login validation.

---
+++ LoginSample/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  LoginSample
+//
+//  Created by Roberto Machorro on 9/11/19.
+//  Copyright © 2019 Roberto Machorro. All rights reserved.
+//
+
+import Cocoa
+
+class LoginViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
